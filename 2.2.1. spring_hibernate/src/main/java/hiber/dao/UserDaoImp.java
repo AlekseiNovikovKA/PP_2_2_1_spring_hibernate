e6c24a794a05bc3fda84cbaaf7a53b6e6411c72b
AlekseiNovikovKA <Lex123Lex@@>
2022-05-31 20:01:04 +0500

    Решение задачи

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,7 +1,9 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -20,10 +22,23 @@ public class UserDaoImp implements UserDao {
    }
 
    @Override
+   public void addCar(Car car) {
+      sessionFactory.getCurrentSession().save(car);
+   }
+
+   @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
+
+   @Override
+   public User findUser(Car car) {
+      Query<User> query = sessionFactory.getCurrentSession().createQuery("from User u where u.car = :car");
+      query.setParameter("car", car);
+      return (User) query.getSingleResult();
+   }
+
 
 }
